20f51ca64020aaf4c872fb799465bdd70ad5a2ff
William Koller <williamkoller30@gmail.com>
2024-01-25 13:37:45 -0300

    feat: add concat

---
+++ cap03/arrays/references-methods-arrays.js
@@ -0,0 +1,9 @@
+/**
+ * Metodo     | Descricao
+ * concat()   | 
+ *
+ */
+
+const frutas = ['banana', 'maca', 'laranja'];
+const frutas2 = ['melancia', 'melao', 'abacate'];
+console.log(frutas.concat(frutas2));
